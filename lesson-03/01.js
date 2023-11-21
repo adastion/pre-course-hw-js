7e7b5304939019edd81992594af0228a4f9a4def
Andrey Sery <andrey.72006@gmail.com>
2023-11-21 19:47:08 +0300

    task 1 arr filter

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,18 @@
 // code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter((item)=> {
+      if (item === "черный") {
+        return item
+      }
+      if (item === "красный") {
+        return item
+      }
+      if (item === "желтый") {
+        return item
+      }
+    }).join('-')
+  }
+
+console.log(createColorString());
